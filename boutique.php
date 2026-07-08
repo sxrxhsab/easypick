4b0f1d15d1f0d20e7b58f61e10a66b92921be06f
sxrxhsab <sabeursarah66@gmail.com>
2026-07-08 16:09:17 +0100

    Suppression définitive des BOM UTF-8 dans tous les fichiers PHP

--- boutique.php
+++ boutique.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 require_once __DIR__ . '/lang.php';
 require_once __DIR__ . '/db.php';
 
--- supprimer-bom.php
+++ supprimer-bom.php
@@ -1,13 +1,10 @@
 <?php
-// supprimer-bom.php - Supprime les BOM UTF-8 de tous les fichiers .php
-
 function removeBOM($dir) {
     $iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($dir));
     foreach ($iterator as $file) {
         if ($file->isFile() && $file->getExtension() === 'php') {
             $path = $file->getPathname();
             $content = file_get_contents($path);
-            // Vérifier si le fichier commence par BOM (EF BB BF)
             if (strpos($content, "\xEF\xBB\xBF") === 0) {
                 $content = substr($content, 3);
                 file_put_contents($path, $content);
@@ -16,7 +13,5 @@ function removeBOM($dir) {
         }
     }
 }
-
 removeBOM(__DIR__);
 echo "🎉 Terminé !\n";
-?>
