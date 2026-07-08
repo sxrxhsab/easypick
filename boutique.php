fab6943f6b6e1c3787d85afc7be77200ca886eaf
sxrxhsab <sabeursarah66@gmail.com>
2026-07-08 01:07:42 +0100

    Description de ce que tu as modifié

--- boutique.php
+++ boutique.php
@@ -1,4 +1,7 @@
 <?php
+// Activer l'affichage des erreurs
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 session_start();
 require_once 'db.php';
 
---
+++ test-env.php
@@ -0,0 +1,7 @@
+<?php
+echo "DB_HOST = " . getenv('DB_HOST') . "<br>";
+echo "DB_PORT = " . getenv('DB_PORT') . "<br>";
+echo "DB_NAME = " . getenv('DB_NAME') . "<br>";
+echo "DB_USER = " . getenv('DB_USER') . "<br>";
+echo "DB_PASSWORD = " . getenv('DB_PASSWORD') . "<br>";
+?>
